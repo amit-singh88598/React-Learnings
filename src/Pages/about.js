f8c200e6c9b48e127bcf4b3156e9c545803149e7
amit-singh88598 <amitsingh992728@gmail.com>
2021-12-09 11:06:04 +0530

    implemented api but data is not showing only printed in console

--- src/Pages/about.js
+++ src/Pages/about.js
@@ -1,11 +1,38 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function About(props) {
+  const [response, setResponse] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    setResponse(res);
+    console.log(res);
+    if (res) {
+      return { res };
+    } else {
+      console.log("Data not Fetched");
+    }
+  };
   return (
     <div>
       <h2>This is About Page</h2>
       <Link to="/">Home</Link>
+      {response.data &&
+        response.data.map((item, index) => {
+          <div key={index}>
+            <div> hello</div>
+            <p>{item.body}</p>
+            <h1>{item.id}</h1>
+            <h3>{item.title}</h3>
+            <h2>{item.userId}</h2>
+          </div>;
+        })}
     </div>
   );
 }
